cfb04870830dcb187f5a20c9a4fd7f6aaf027766
DowlathBashaG <9671419+DowlathBashaG@users.noreply.github.com>
2025-04-14 08:09:57 +0530

    Add files via upload
    Initial Commit

---
+++ SideCar-Pattern/SideCar-Design-Pattern/book-service/src/main/java/dowlath/io/dto/Book.java
@@ -0,0 +1,14 @@
+package dowlath.io.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+}
---
+++ SideCar-Pattern/SideCar-Design-Pattern/book-service/src/main/java/dowlath/io/service/BookService.java
@@ -0,0 +1,22 @@
+package dowlath.io.service;
+
+import dowlath.io.dto.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private List<Book> books = new ArrayList<>();
+
+    public Book addBook(Book book) {
+        books.add(book);
+        return book;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+}
